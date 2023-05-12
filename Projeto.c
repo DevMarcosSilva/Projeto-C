cfd84db60b8e9c8082dd28316b6d6afd87b9dd62
marcosgs <mvinicius17112002@gmail.com>
2023-05-11 23:15:52 -0300

    add vacinação

--- Projeto.c
+++ Projeto.c
@@ -3,7 +3,7 @@
 
     void  coletadedados (char nome[][40], char email[][20], char sexo[][11], char endereco[][30] ){
         int i;
-        for ( i = 0; i < 2; i++)
+        for ( i = 0; i < ; i++)
         {
             printf("Informe o nome completo do %d usuario: ",i+1);
             fgets(nome[i], 40, stdin);
@@ -27,21 +27,37 @@
         }
         
     }
-    
+     int vacina(int vacinacao[][1]){
+            int i;
+            for ( i = 0; i < 5; i++)
+            {
+             printf("o usuario %d foi vacinado ? 1-Sim ou 2-Nao",i+1);
+            scanf("%d",&vacinacao[i]);
+            }
+            
+            
+     }
 
     int main()
     {
-        char nome[2][40],email[2][20], sexo[2][11],endereco[2][30];
-        int i;
+        char nome[5][40],email[5][50], sexo[5][11],endereco[5][30];
+        int i,vacinacao[5];
         
         coletadedados(nome,email,sexo,endereco);
-        
-        for (i = 0; i < 2; i++)
+        vacina(vacinacao);
+        printf("%d",vacinacao);
+        for (i = 0; i < 5; i++)
         {
             printf("\no nome do %d uruario e %s\n",i+1, nome[i]);
             printf("\no email do %d usuario e %s\n",i+1,email[i]);
             printf("\no sexo do %d usuario e %s\n",i+1,sexo[i]);
             printf("\no endereco do %d usuraio e %s\n",i+1,endereco[i]);
+            if (vacinacao[i] == 1)
+            {
+                printf("o usuario %d foi vacinado ",i+1);
+            }else printf("o usuario %d nao foi vacinado ",i+1);
+            
+            
         }
         
        
