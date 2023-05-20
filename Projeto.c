f6f0a23b885f638403ca9bf3c06eb05b1619cb9f
marcosgs <mvinicius17112002@gmail.com>
2023-05-20 19:50:39 -0300

    ajustes

--- Projeto.c
+++ Projeto.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<string.h>
-
-    void  coletadedados (char nome[][40], char email[][20], char sexo[][11], char endereco[][30] ){
-        int i;
+#include<time.h>
+#include<stdlib.h>
+    
+    void  coletadedados ( int vacinacao[][1],char nome[][40], char email[][20], char sexo[][11], char endereco[][30], double altura[][1]){
+            int i;
         for ( i = 0; i < 2; i++)
-        {
+        {   fflush(stdin);
             printf("Informe o nome completo do %d usuario: ",i+1);
             fgets(nome[i], 40, stdin);
             fflush(stdin);
@@ -15,7 +17,7 @@
             system("cls");
             do
             {
-             printf("informe o sexo do %d usuario: feminino masculino ou indiferente ",i+1);
+             printf("informe o sexo do %d usuario: Feminino Masculino ou Iindiferente ",i+1);
              scanf("%s",sexo[i]);
              fflush(stdin);
             } while (strcmp(sexo[i], "masculino") !=0 && strcmp(sexo[i], "feminino")!=0&& strcmp(sexo[i], "indiferente")!=0);
@@ -23,51 +25,79 @@
             printf("Agora informe o endereço do usuraio %d",i+1);
             fgets(endereco[i],30,stdin);
             fflush(stdin);
-            
+            printf("o usuario %d foi vacinado ? 1-Sim ou 2-Nao",i+1);
+            scanf("%d",vacinacao[i]);
+            printf("Informe a altura do usuario %d",i+1);
+                scanf("%lf",&altura[i]);
         }
         
     }
-     int vacina(int vacinacao[][1]){
+     
+            
+            
+     
+     double coletaltura(double altura[][1]){
             int i;
             for ( i = 0; i < 2; i++)
-            {
-             printf("o usuario %d foi vacinado ? 1-Sim ou 2-Nao",i+1);
-            scanf("%d",&vacinacao[i]);
+            {   
+                printf("Informe a altura do usuario %d",i+1);
+                scanf("%lf",&altura[i]);
             }
-            
-            
+           
      }
+     int idaleatorio(int id[]){
+        int i,idd;
+        srand(time(NULL));
+        for ( i = 0; i < 2; i++)
+        {
+           
+           idd=1000+rand()%5000;
+           id[i]=idd;
+           printf("o id do %d usuario e: %d",i+1,id[i]);
+           
+           
+            
+        }
+        return id;
 
+     }
+    
     int main()
     {
         char nome[5][40],email[5][50], sexo[5][11],endereco[5][30];
-        int i,vacinacao[5];
-        
-        coletadedados(nome,email,sexo,endereco);
-        vacina(vacinacao);
-        printf("%d",vacinacao);
-        for (i = 0; i < 2; i++)
-        {
-            printf("\no nome do %d uruario e %s\n",i+1, nome[i]);
-            printf("\no email do %d usuario e %s\n",i+1,email[i]);
-            printf("\no sexo do %d usuario e %s\n",i+1,sexo[i]);
-            printf("\no endereco do %d usuraio e %s\n",i+1,endereco[i]);
-            if (vacinacao[i] == 1)
+        int i,vacinacao[5],id[2][5],opcao[1],escolha;
+        double altura[2];
+        do
+        {printf("ola usuario seja bem vindo\ninform qual processo deseja realizar!\n ");
+        printf("1-incluir um usuário\n");
+        printf("1-edite um usuário\n");
+        printf("2-exclua um usuário\n");
+        printf("4-busque um usuário pelo email\n");
+        printf("5-imprima todos os usuários cadastrados\n");
+        printf("6-faça backup dos usuários cadastrados\n");
+        printf("7-faça restauração dos dados");
+        scanf("%d",&escolha);
+            switch (escolha)
             {
-                printf("o usuario %d foi vacinado(a) ",i+1);
-            }else printf("o usuario %d nao foi vacinado(a) ",i+1);
-            
-            
-        }
+            case 1:
+                coletadedados(nome,email,sexo,endereco,vacinacao,altura);
+                
+                break;
+            case 2:
+                
+            default:
+                break;
+            }
+                } while (escolha==1);
+        inicio(opcao);
+        coletadedados(nome,email,sexo,endereco,vacinacao,altura);
         
-       
-            
-            
-            
-        
+        coletaltura(altura);
+        idaleatorio(id);
+        printf("%d",vacinacao);
         
         
         return 0;
     }
     
-    
+     
